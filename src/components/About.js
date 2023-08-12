b22f51a26208580fe00030cc1c1cc18ef30f4b72
SMJanani <smjanani2504@gmail.com>
2023-08-12 22:05:13 +0530

    project done

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/about.css";
+function About() {
+  return (
+    <div  className='aboutheading'>
+        <h1>About</h1>
+        <p>
+            lfdvlifdjvijfdvjfdjfdgjfdlvbfljhf
+            fkrijgijrgijrtigjtflkjgljkrljlrlj
+        </p>
+    </div>
+  )
+}
+
+export default About
